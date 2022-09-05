b0c9ef8beead99e09ce48244112e93ff6c799099
Gaurangkothiya <gaurangkothiya1234@gmail.com>
2022-09-05 09:07:19 +0530

    'Added Navbar component'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Navbar from './pages/NavBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePost from './pages/CreatePost';
 
-var id = process.env.FIREBASE_API_KEY
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/createpost' element={<CreatePost />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/createpost' element={<CreatePost />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
     </Router>
   );
 }
